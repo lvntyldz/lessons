689b992be46e869b4a9dc5a4ac57536651c23a58
Levent YILDIZ <levent.yildiz@ardictech.com>
2021-01-02 11:48:41 +0300

    add asynstroage support

--- MMOT225-Cloud/12.hafta/LocalStorageExample/src/Home.js
+++ MMOT225-Cloud/12.hafta/LocalStorageExample/src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, Button, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class Home extends React.Component {
 
@@ -23,7 +23,7 @@ export default class Home extends React.Component {
         userList.push(user);
 
 
-        await AsyncStorage.setItem(JSON.stringify(userList));
+        await AsyncStorage.setItem("USERS", JSON.stringify(userList));
 
         console.warn("add user - user : ", user);
 
@@ -31,14 +31,34 @@ export default class Home extends React.Component {
 
     }
 
+    handleListOnPress = async () => {
+
+        const userList = await AsyncStorage.getItem("USERS");
+        console.warn("userList : ", userList);
+        this.setState({ users: JSON.parse(userList) });
+    }
+
+    handleDeleteOnPress = () => {
+
+        AsyncStorage.removeItem("USERS").then(r => {
+            this.setState({ users: [] });
+        }).catch(e => {
+            console.warn("error : ", e);
+        })
+
+    }
+
+
     render() {
 
         return (<View style={{ margin: 20 }}>
             <Button title="add user" onPress={() => this.handleOnPress()} />
+            <Button title="list user" onPress={() => this.handleListOnPress()} />
+            <Button title="delete user" onPress={() => this.handleDeleteOnPress()} />
 
             {
                 this.state.users.map((v, k) => {
-                    return <Text>{v.firstname} - {v.lastname}</Text>
+                    return <Text key={k} >{v.firstname} - {v.lastname}</Text>
                 })
 
             }
